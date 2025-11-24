c2414a9a1a726ee85f6d5b907807d44492829d62
unknown <marko.omil2016@gmail.com>
2025-11-24 03:35:14 +0200

    added an auth-navbar, sign-up, login, forgot-password and otp-verify components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { ServicesComponent } from './components/services/services.component';
+import { PregnancyHealthComponent } from './components/pregnancy-health/pregnancy-health.component';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
+import { VerifyOtpComponent } from './components/verify-otp/verify-otp.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: AuthLayoutComponent,
+    children: [
+      { path: '', redirectTo: 'login', pathMatch: 'full' },
+      { path: 'login', component: LoginComponent },
+      { path: 'signup', component: SignUpComponent },
+      { path: 'services', component: ServicesComponent },
+      { path: 'pregnancy-health', component: PregnancyHealthComponent },
+      { path: 'forgot-password', component: ForgotPasswordComponent },
+      { path: 'verify-otp', component: VerifyOtpComponent },
+    ],
+  },
+
+  { path: '**', component: NotFoundComponent },
+];
---
+++ src/app/components/pregnancy-health/pregnancy-health.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pregnancy-health',
+  standalone: true,
+  imports: [],
+  templateUrl: './pregnancy-health.component.html',
+  styleUrl: './pregnancy-health.component.scss'
+})
+export class PregnancyHealthComponent {
+
+}
